362821604e70e647cfeda402c5e8b058a9fc2b8a
Leon Klute <leonklute@live.nl>
2023-11-21 15:01:19 +0100

    split python runs right

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -4,14 +4,9 @@ local function run_curr_python_file()
     local file_name = vim.api.nvim_buf_get_name(0)
 
     -- Get terminal codes for running python file
-    -- ("i" to enter insert before typing rest of the command)
-    local py_cmd = vim.api.nvim_replace_termcodes("ipython \"" .. file_name .. "\"<cr>", true, false, true)
+    local py_cmd = vim.api.nvim_replace_termcodes("ipython \"" .. file_name .. "\"<cr><C-\\><C-N>G", true, false, true)
 
-    -- Determine terminal window split and launch terminal
-    local percent_of_win = 0.4
-    local curr_win_height = vim.api.nvim_win_get_height(0) -- Current window height
-    local term_height = math.floor(curr_win_height * percent_of_win) -- Terminal height
-    vim.cmd(":below " .. term_height .. "split | term") -- Launch terminal (horizontal split)
+    vim.cmd(":vert split | term") -- Launch terminal (vertical split)
 
     -- Press keys to run python command on current file
     vim.api.nvim_feedkeys(py_cmd, "t", false)
@@ -24,25 +19,21 @@ local function test_curr_python_file()
 
     -- Get terminal codes for running python file
     -- ("i" to enter insert before typing rest of the command)
-    local py_cmd = vim.api.nvim_replace_termcodes("ipython -m unittest \"" .. file_name .. "\"<cr>", true, false, true)
+    local py_cmd = vim.api.nvim_replace_termcodes("ipython -m unittest \"" .. file_name .. "\"<cr><C-\\><C-N>G", true,
+        false, true)
 
-    -- Determine terminal window split and launch terminal
-    local percent_of_win = 0.4
-    local curr_win_height = vim.api.nvim_win_get_height(0) -- Current window height
-    local term_height = math.floor(curr_win_height * percent_of_win) -- Terminal height
-    vim.cmd(":below " .. term_height .. "split | term") -- Launch terminal (horizontal split)
+    vim.cmd(":vert split | term") -- Launch terminal (vertical split)
 
     -- Press keys to run python command on current file
     vim.api.nvim_feedkeys(py_cmd, "t", false)
 end
 
-vim.keymap.set({'n'}, '<F5>', '', {
+vim.keymap.set({ 'n' }, '<F5>', '', {
     desc = "Run .py file via Neovim built-in terminal",
     callback = run_curr_python_file
 })
 
-vim.keymap.set({'n'}, '<F6>', '', {
+vim.keymap.set({ 'n' }, '<F6>', '', {
     desc = "Run unittests in .py file via Neovim built-in terminal",
     callback = test_curr_python_file
 })
-
